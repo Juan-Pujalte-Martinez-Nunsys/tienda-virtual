9906573bafa6acdd5bba699c9413de5c256e4b3e
juan.pujalte <juan.pujalte@nunsys.com>
2025-04-11 11:13:45 +0200

    added about-us component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'categories', component: CategoriesComponent}
+    { path: 'categories', component: CategoriesComponent},
+    { path: 'about-us', component: AboutUsComponent }
 ];
