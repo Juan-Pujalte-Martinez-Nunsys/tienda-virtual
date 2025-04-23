6679da33e68e352c8948adba2f1b502d078c4480
juan.pujalte <juan.pujalte@nunsys.com>
2025-04-23 10:20:03 +0200

    navbar store submenu now loads all categories dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Category } from './models/category.model';
+import { CategoriesService } from './apis/categories.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,17 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tienda-virtual';
+  categories?: Category[];
+  
+    constructor(
+      private categoriesService: CategoriesService
+    ) {}
+
+    ngOnInit(): void {
+      this.categoriesService.getAll().subscribe(
+        categories => this.categories = categories
+      );
+    }
 }
